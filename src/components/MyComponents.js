2c774733d1de7a4b0cda92da7ae49285fc327785
PHUC NGUYEN <nguyenngocphuc1712@gmail.com>
2023-02-02 08:56:43 +0700

    video16

---
+++ src/components/MyComponents.js
@@ -0,0 +1,15 @@
+// class components
+// function components
+
+import React from "react";
+class MyComponents extends React.Component{
+    render() {
+        return(
+            //JSX
+                <div>My first Components
+                    {Math.random()}
+                </div>
+        )
+    }
+}
+export default MyComponents;
