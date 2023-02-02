31bd93fcfe2a45d0fffa174e8f8c65586a852bcf
PHUC NGUYEN <nguyenngocphuc1712@gmail.com>
2023-02-02 11:11:40 +0700

    bai 20

--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -8,11 +8,21 @@ class MyComponents extends React.Component{
         age : 23,
         address :"Quang Nam"
     }
+    handleClick ()
+    {
+        console.log("Click my button")
+    }
+    handleOnMouse(event)
+    {
+        console.log(event.target)
+    }
     render() {
         return(
             //JSX
                 <div>My first Component
                     My nam is {this.state.name} and I'm live in {this.state.address};
+                    <button onClick={this.handleClick}>Click me</button>
+                    <button onMouseOver={this.handleOnMouse}>Hover me</button>
                 </div>
         )
     }
